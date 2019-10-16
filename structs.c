3f878d10c784f234676aafc9db3f6b0e39e755a7
Angelique <acharles-davis00@stuy.edu>
2019-10-16 10:11:48 -0400

    added random function and main

--- structs.c
+++ structs.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <time.h>
 struct first{
   char *a;
   double b; //vault
@@ -15,29 +16,39 @@ int changename(struct first *r, char *name){
 }
 int changescore(struct first *p, char *event, double score){
 if (strcmp(event,"vault")==0){
-  //*(s.b)=score;
-  p->b=score;
-}
+  p->b=score;}
 if (strcmp(event,"bars")==0){
-//  *(s.d)=score;
-p->d=score;
-}
+p->d=score;}
 if (strcmp(event,"beam")==0){
-  //*(s.f)=score;
-  p->f=score;
-}
+  p->f=score;}
 if (strcmp(event,"floor")==0){
-//  *(s.h)=score;
-p->h=score;
-}
+p->h=score;}
   p->j=p->b+p->d+p->f+p->h;//adjusts all-around score
   return 1;
+}
+int changescores(struct first *p,double vault, double bars, double beam, double floor){
+    p->b=vault;
+  p->d=bars;
+    p->f=beam;
+  p->h=floor;
+    p->j=p->b+p->d+p->f+p->h;//adjusts all-around score
+    return 1;
 }
 int printstats(struct first *p){
   printf("Name: %s\nVault:%f\nBars:%f\nBeam:%f\nFloor:%f\nAll-around:%f\n",p->a,p->b,p->d,p->f,p->h,p->j);
   return 1;
 }
-//int getvault()
+int makerandom(struct first *p){
+srand((long)time(NULL));
+int a =rand()%5;
+if (a==0) p->a="Alice";
+if (a==1) p->a="Mckenzie";
+if (a==2) p->a="Barbara";
+if (a==3) p->a="Julie";
+if (a==4) p->a="Jamie";
+changescores(p,7.1+0.1*(rand()%30),7.1+0.1*(rand()%30),7.1+0.1*(rand()%30),7.1+0.1*(rand()%30));
+return 1;
+}
 int main(){
 struct first a;
 struct first *toa = &a;
@@ -45,9 +56,9 @@ struct first b;
 struct first *tob = &b;
 struct first c;
 struct first *toc = &c;
-printf("The Struct shows a gymnast's name, along with her scores in vault, bars, beam, floor and all-around");
+printf("The struct shows a gymnast's name, along with her scores in vault, bars, beam, floor and all-around");
 printf("\nScores are on a scale from 1 to 10, 10 being a perfect score. All-around scores are on a scale from 4 to 40 and are the sum of the other scores");
-printf("\nSetting Linda's scores to 9.5, 9, 7.8 and 8.9\n");
+printf("\n\nSetting Linda's scores to 9.5, 9, 7.8 and 8.9\n");
 printf("Printing her stats: \n");
 changename(toa, "Linda");
 changescore(toa,"vault",9.5);
@@ -55,5 +66,12 @@ changescore(toa,"bars",9);
 changescore(toa,"beam",7.8);
 changescore(toa,"floor",8.9);
 printstats(toa);
+printf("\n\nSetting just her vault score to 8.4\n");
+changescore(toa,"vault",8.4);
+printf("Printing her stats: \n");
+printstats(toa);
+printf("\n\nCreating a new random athlete and printing her stats:\n");
+makerandom(tob);
+printstats(tob);
 return 1;
 }
